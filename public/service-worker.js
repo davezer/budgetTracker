711eae263582c1f5a077f8b20111c494cc4a7b98
davezer <davezer27@gmail.com>
2022-02-23 18:10:38 -0500

    fixing service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,6 @@
 
 // global constants 
-const APP_PREFIX = 'FoodFest-';     
+const APP_PREFIX = 'budgetTracker-';     
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
 
@@ -21,48 +21,16 @@ const FILES_TO_CACHE = [
   '/icons/icon-512x512.png'  
 ];
 
-// adding files to the precache so that the application can use the cache (installing service worker)
-self.addEventListener('install', function (e) {
-  e.waitUntil(
-      caches.open(CACHE_NAME).then(function (cache) {
-        console.log('installing cache : ' + CACHE_NAME)
-        return cache.addAll(FILES_TO_CACHE)
-      })
-    )
-});
-
-// activate a service worker
-self.addEventListener('activate', function (e) {
-  e.waitUntil(
-    caches.keys().then(function (keyList) {
-      let cacheKeeplist = keyList.filter(function (key) {
-        return key.indexOf(APP_PREFIX);
-      });
-          cacheKeeplist.push(CACHE_NAME);
-          // returns a promise that resolves once all old versions of the cache have been deleted 
-          return Promise.all(keyList.map(function (key, i) {
-              if (cacheKeeplist.indexOf(key) === -1) {
-              console.log('deleting cache : ' + keyList[i] );
-              return caches.delete(keyList[i]);
-              }
-          })
-      );
-  })
-  )
-});
-
-// retrieving information from the cache 
+// Respond with cached resources
 self.addEventListener('fetch', function (e) {
   console.log('fetch request : ' + e.request.url)
   e.respondWith(
     caches.match(e.request).then(function (request) {
       if (request) { // if cache is available, respond with cache
         console.log('responding with cache : ' + e.request.url)
-      //   console.log(e.request.url)
         return request
       } else {       // if there are no cache, try fetching request
         console.log('file is not cached, fetching : ' + e.request.url)
-      //   console.log(e.request.url)
         return fetch(e.request)
       }
 
@@ -70,4 +38,38 @@ self.addEventListener('fetch', function (e) {
       // return request || fetch(e.request)
     })
   )
+})
+
+// Cache resources
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log('installing cache : ' + CACHE_NAME)
+      return cache.addAll(FILES_TO_CACHE)
+    })
+  )
+})
+
+// Delete outdated caches
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      // `keyList` contains all cache names under your username.github.io
+      // filter out ones that has this app prefix to create keeplist
+      let cacheKeeplist = keyList.filter(function(key) {
+        return key.indexOf(APP_PREFIX);
+      });
+      // add current cache name to keeplist
+      cacheKeeplist.push(CACHE_NAME);
+
+      return Promise.all(
+        keyList.map(function(key, i) {
+          if (cacheKeeplist.indexOf(key) === -1) {
+            console.log('deleting cache : ' + keyList[i]);
+            return caches.delete(keyList[i]);
+          }
+        })
+      );
+    })
+  );
 });
